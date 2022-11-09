7e95f10564f0e9f67468914c375cda2448276585
arielgoes <arielgoesdecastro@gmail.com>
2022-11-09 12:10:08 -0300

    asd

--- FRR/runAll.sh
+++ FRR/runAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAX_ITER=1
+MAX_ITER=30
 echo "Killing PREVIOUS controller terminal..."
 sudo pkill -f controller.py
 echo "Killing PREVIOUS packet injection..."
@@ -8,11 +8,10 @@ sudo pkill -f send_socket_path_id_0_multiple_packets.py
 sudo pkill -f send_socket_path_id_1_multiple_packets.py
 sudo pkill -f receive.py
 
-#A=('s1' 's2' 's3' 's4' 's5' 's1')
-A=('s5' 's1')
-#TIME_OUTS="10000 20000 30000 40000 50000 60000 70000 80000 90000 100000"
-#TIME_OUTS="10000"
-TIME_OUTS="20000"
+A=('s1' 's2' 's3' 's4' 's5' 's1')
+#A=('s5' 's1')
+TIME_OUTS="10000 20000 30000 40000 50000 60000 70000 80000 90000 100000"
+#TIME_OUTS="30000"
 
 for k in $TIME_OUTS; do
 	for ((j=0; j<${#A[@]}-1; j++)); do
@@ -29,7 +28,7 @@ for k in $TIME_OUTS; do
 			sudo /home/p4/mininet/util/m h2 python snd-rcv_scripts/receive.py &
 			echo "Inject packets..."
 			sudo /home/p4/mininet/util/m h1 python snd-rcv_scripts/send_socket_path_id_0_multiple_packets.py &
-			sudo /home/p4/mininet/util/m h1 python snd-rcv_scripts/send_socket_path_id_1_multiple_packets.py &
+			#sudo /home/p4/mininet/util/m h1 python snd-rcv_scripts/send_socket_path_id_1_multiple_packets.py &
 			echo "----------------------------------------ITERATION $i/$MAX_ITER----------------------------------------"
 			sleep 8
 			echo "Killing packet injection..."
--- FRR/runTest.sh
+++ FRR/runTest.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Sending a bunch of packets..."
+
 sudo /home/p4/mininet/util/m h1 python snd-rcv_scripts/send_socket_path_id_0_multiple_packets.py &
 sudo /home/p4/mininet/util/m h1 python snd-rcv_scripts/send_socket_path_id_1_multiple_packets.py &
-sleep 20
+
+sleep 15
 echo "Killing..."
 sudo pkill -f send_socket_path_id_0_multiple_packets.py
 sudo pkill -f send_socket_path_id_1_multiple_packets.py
